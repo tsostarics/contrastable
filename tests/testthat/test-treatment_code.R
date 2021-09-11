b763a4fc9cdacf464897a80dda5920fba90dede8
Thomas Sostarics <tsostarics@gmail.com>
2021-09-11 11:06:45 -0500

    add tests for treatment_code and sum_code wrappers

---
+++ tests/testthat/test-treatment_code.R
@@ -0,0 +1,12 @@
+test_that("Treatment_code and contr.treatment are equivalent", {
+  tst <- data.frame(fac = factor(c('a','b','c')))
+  ref_mat <- matrix(c(1,0,0,0,0,1), nrow = 3)
+  colnames(ref_mat) <- c('a','c')
+  rownames(ref_mat) <- c('a','b','c')
+
+  ref_cont <- enlist_contrasts(tst, fac ~ contr.treatment + "b", verbose = FALSE)
+  test_cont <- enlist_contrasts(tst, fac ~ treatment_code + "b", verbose = FALSE)
+
+  expect_equal(ref_cont, test_cont)
+  expect_equal(test_cont[[1L]], ref_mat)
+})
