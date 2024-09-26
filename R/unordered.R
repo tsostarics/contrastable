0c7e2a72f896de6856ae79c05d0997804b719c10
Thomas Sostarics <tsostarics@gmail.com>
2024-09-26 13:07:59 -0500

    remove copied factor implementation in is.unordered

--- R/unordered.R
+++ R/unordered.R
@@ -51,14 +51,6 @@ as.unordered <- function(x) {
     class(x) <- "factor"
     x
     # nocov start
-  } else if (!is.object(x) && is.integer(x)) {
-    levels <- sort.int(unique.default(x))
-    f <- match(x, levels)
-    levels(f) <- as.character(levels)
-    if (!is.null(nx <- names(x)))
-      names(f) <- nx
-    class(f) <- "factor"
-    f
   } else {
     factor(x)
   }
